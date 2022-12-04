e0313455f6379cc7751042e01e86560562dda6bd
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2022-12-04 19:52:56 +0400

    Create pyrogram_modul.py

---
+++ pyrogram_modul.py
@@ -0,0 +1,33 @@
+from pyrogram import Client, filters, idle
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton #düyməsi yoxdur.!
+import pyrogram
+from Config import Config
+from datetime import datetime
+from telethon import Button
+
+
+app = Client(
+    "User Tag Bot",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN,
+)
+
+#---------------------------------------------------------------GROUP GIREKEN SALAMLAMA MSJ------------------------------------------------------------------------------#
+@app.on_message(filters.new_chat_members, group=1)
+async def hg(bot: Client, msg: Message):
+    for new_user in msg.new_chat_members:
+        if str(new_user.id) == str(Config.BOT_ID):
+            await msg.reply(
+                f'''`Salam` {msg.from_user.mention} `Məni` {msg.chat.title} `Qrupa əlavə etdiyiniz üçün təşəkkürlər⚡️` \n\n **🤖Qruplardakı userləri tag Edmə üçün Yaradıldım.\n🤖Kömək üçün /start yazmaq kifayətdir.✨**''')
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+
+#-------------------------------------------------------------OWNERS SALAMLAMA MSJ---------------------------------------------------------------------------------------#
+        elif str(new_user.id) =="5540993505":
+            await msg.reply('🤖 [Ədalət 𝗧𝗮𝗴𝗴𝗲𝗿](https://t.me/EdaletSup)-un Sahibi, Qrupa Qatıldı.\n Xoş Gəldin  Aramıza Sahib, Necəsən?🥰.')
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+app.start()
+print(f"Bot poroqramı ( {pyrogram.__version__} ilə başladı... Botu işə salın!")
+idle()
